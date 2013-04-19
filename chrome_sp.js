269963b3963404cb6ec4d1a09c815aa36c1e91df
Mike Elston <mike@indstry.com>
2013-04-19 11:20:35 -0400

    code cleanup

--- chrome_sp.js
+++ chrome_sp.js
@@ -14,16 +14,14 @@ function isSharepoint() {
 }
 
 if(isSharepoint()){
-  var spinjectjs = ""
-  spinjectjs = spinjectjs + "var jsInitChecktimer = setInterval(checkForJS_Finish, 1000);\n"
-  spinjectjs = spinjectjs + "function checkForJS_Finish(){\n"
-  spinjectjs = spinjectjs + "  // console.log(jsInitChecktimer + ' ' + document.getElementById('s4-workspace').getAttribute('style') + ' ' + typeof _spBodyOnLoadWrapper)\n"
-  spinjectjs = spinjectjs + "  if(typeof _spBodyOnLoadWrapper == 'function' && document.getElementById('s4-workspace').getAttribute('style') == null) {\n"
-  spinjectjs = spinjectjs + "    clearInterval(jsInitChecktimer);\n"
-  spinjectjs = spinjectjs + "    // console.log('fixed sp');\n"
-  spinjectjs = spinjectjs + "    _spBodyOnLoadWrapper();\n"
-  spinjectjs = spinjectjs + "  }\n"
-  spinjectjs = spinjectjs + "}\n"
+  var spinjectjs = "var jsInitChecktimer = setInterval(checkForJS_Finish, 1000); \
+                      function checkForJS_Finish(){ \
+                        // console.log(jsInitChecktimer + ' ' + document.getElementById('s4-workspace').getAttribute('style') + ' ' + typeof _spBodyOnLoadWrapper) \
+                        if(typeof _spBodyOnLoadWrapper == 'function' && document.getElementById('s4-workspace').getAttribute('style') == null) { \
+                          clearInterval(jsInitChecktimer); \
+                          _spBodyOnLoadWrapper(); \
+                        } \
+                      }"
   var s = document.createElement("script");
   s.type = "text/javascript";
   s.innerHTML = spinjectjs;
